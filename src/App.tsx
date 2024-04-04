a91e06dd917af59976681d53aaec83f397964e21
Pashavol <pavel.volobuev95@yandex.ru>
2024-04-04 11:15:26 +0300

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Theme, presetGpnDefault } from "@consta/uikit/Theme";
+
+
+function App() {
+  return (
+  <Theme preset={presetGpnDefault}>
+    Consta starter для дизайнеров.
+  </Theme>
+  )
+}
+
+export default App
